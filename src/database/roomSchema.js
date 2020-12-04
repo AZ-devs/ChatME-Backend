776cd218209e3f3aecf7e32d4c664d076fe7cf81
Abdallah-Zakaria <abdallah.zakaria1996@icloud.com>
2020-12-04 17:46:02 +0200

    socketEvent

---
+++ src/database/roomSchema.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const room = mongoose.model('room',
+  mongoose.Schema({
+    islocked: { type: Boolean, default: false },
+    name: { type: String, required: true },
+    password: { type: String, default: '' },
+    messages: { type: [] },
+    pepole: { type: [] },
+    date: { type: Date, default: Date.now },
+  }),
+);
+
+module.exports = mongoose.model('room', room);
