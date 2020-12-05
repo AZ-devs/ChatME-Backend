4c992f15db96a0530c9ba4ebf15519e8ac545a32
Abdallah-Zakaria <abdallah.zakaria1996@icloud.com>
2020-12-05 16:08:26 +0200

    handleLobby

--- src/socket.io/chat.js
+++ src/socket.io/chat.js
@@ -4,21 +4,26 @@ const collection = require('../database/collection');
 const { chat } = require('../server');
 
 chat.on('connection', (socket) => {
-  console.log(socket.id,'connected');
+  console.log(socket.id, 'connected');
 
   socket.on('join', async (payload) => {
     socket.exitHandler = { name: payload.name, roomID: payload.roomID };
     const messages = await collection.join(payload.roomID, payload);
     if (messages) {
-      console.log(socket.id,' joined room:', payload.roomID);
+      console.log(socket.id, ' joined room:', payload.roomID);
       socket.join(payload.roomID);
       chat.to(socket.id).emit('messages', messages);
+      let roomsDetails = await collection.allRooms();
+      chat.emit('lobby', roomsDetails);
     }
+
   });
 
-  socket.on('exit', (payload) => {
-    collection.exit(payload.roomID, payload);
+  socket.on('exit', async (payload) => {
+    await collection.exit(payload.roomID, payload);
     socket.leave(payload.roomID);
+    let roomsDetails = await collection.allRooms();
+    chat.emit('lobby', roomsDetails);
   });
 
   socket.on('sendMessage', (payload) => {
@@ -31,14 +36,16 @@ chat.on('connection', (socket) => {
     socket.exitHandler = { name: payload.name, roomID };
     await collection.join(roomID, payload);
     socket.join(roomID);
+    let roomsDetails = await collection.allRooms();
+    chat.emit('lobby', roomsDetails);
   });
 
-  socket.on('disconnect', () => {
+  socket.on('disconnect', async () => {
     if (socket.exitHandler) {
-      collection.exit(socket.exitHandler.roomID, socket.exitHandler);
+      await collection.exit(socket.exitHandler.roomID, socket.exitHandler);
+      let roomsDetails = await collection.allRooms();
+      chat.emit('lobby', roomsDetails);
     }
-    console.log(socket.id,'disconnected');
+    console.log(socket.id, 'disconnected');
   });
-
 });
-
